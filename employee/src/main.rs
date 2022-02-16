9ab3b729d827e7d48358465b15132ab4fb8039fe
SifuF <francisgodden@hotmail.com>
2022-02-16 04:36:54 +0000

    Update employee main.rs

--- employee/src/main.rs
+++ employee/src/main.rs
@@ -1,19 +1,41 @@
+use std::io;
 use std::collections::HashMap;
 
 fn main() {
 
-    let mut v1 = vec!["Sally", "Bob"];
-    v1.sort();
-    let mut v2 = vec!["Amir"];
-    v2.sort();
+    let mut vec = vec![("Engineering", "Sally"), ("Engineering", "Bob"), ("Sales", "Amir")];
+
+    println!("Enter command:");
+
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).expect("Failed to read line!");
+
+    
+    let mut iter = buffer.split_whitespace();
+    if iter.next().unwrap() == "Add" {             //Add
+        let value = iter.next().unwrap();          //Name
+        iter.next();                               //to
+        let key = iter.next().unwrap();            //Department
+        vec.push((key, value));
+    }
+    vec.sort();
 
     let mut map = HashMap::new();
-
-    map.insert(String::from("Sales"), &v2);
-    map.insert(String::from("Engineering"), &v1);
     
-    let query = String::from("Engineering");
-    println!("{} = {:?}", &query, map.get(&query).unwrap());        
+    for (key, _value) in &vec {
+        let empty_vec = Vec::new();
+        map.insert(key, empty_vec);
+    }  
+
+    for (key, value) in &vec {
+        let mut new_value = map.get(key).unwrap().clone();
+        new_value.push(value);
+        map.insert(key, new_value);
+    }  
+    
+    let query = "Engineering";
+    let val = map.get(&query).unwrap();
+    println!("{} = {:?}", query, val);        
 
     let mut v = Vec::new();
     for (key, value) in &map {
